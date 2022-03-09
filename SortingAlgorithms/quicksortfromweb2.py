2f7a6209f7c5442bad6530db445b0067f54f7c74
Rick <rmh_rickhardy@hotmail.com>
2022-03-09 22:19:05 +0000

    sorts

---
+++ SortingAlgorithms/quicksortfromweb2.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 18 13:57:33 2022
+
+@author: richardhardy
+"""
+
+def partition(arr, low, high):
+ 
+    pivot = arr[low]
+    i = low - 1
+    j = high + 1
+ 
+    while (True):
+ 
+        # Find leftmost element greater than
+        # or equal to pivot
+        i += 1
+        while (arr[i] < pivot):
+            i += 1
+ 
+        # Find rightmost element smaller than
+        # or equal to pivot
+        j -= 1
+        while (arr[j] > pivot):
+            j -= 1
+ 
+        # If two pointers met.
+        if (i >= j):
+            return j
+ 
+        arr[i], arr[j] = arr[j], arr[i]
+ 
+ 
+''' The main function that implements QuickSort
+arr --> Array to be sorted,
+low --> Starting index,
+high --> Ending index '''
+ 
+ 
+def quickSort(arr, low, high):
+    ''' pi is partitioning index, arr[p] is now
+    at right place '''
+    if (low < high):
+ 
+        pi = partition(arr, low, high)
+ 
+        # Separately sort elements before
+        # partition and after partition
+        quickSort(arr, low, pi)
+        quickSort(arr, pi + 1, high)
+ 
+ 
+''' Function to pran array '''
+ 
+ 
+def printArray(arr, n):
+    for i in range(n):
+        print(arr[i], end=" ")
+    print()
+ 
+ 
+# Driver code
+arr = [10, 7, 8, 9, 1, 5]
+n = len(arr)
+quickSort(arr, 0, n - 1)
+print("Sorted array:")
+printArray(arr, n)
+ 
+# This code is contributed by shubhamsingh10
